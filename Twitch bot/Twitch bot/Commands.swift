a8387d8f919b5a390322275701755a77d46b5270
Leonardo Luiz <lcoutinholuiz@gmail.com>
2024-03-17 12:33:46 -0300

    Pushing whistle file sound

--- Twitch bot/Twitch bot/Commands.swift
+++ Twitch bot/Twitch bot/Commands.swift
@@ -11,160 +11,6 @@ import AVFoundation
 
 import Foundation
 
-class IRCClient {
-    let host: String
-    let port: Int
-    var inputStream: InputStream!
-    var outputStream: OutputStream!
-
-    init(host: String, port: Int) {
-        self.host = host
-        self.port = port
-    }
-
-    func connect() {
-        Stream.getStreamsToHost(withName: host, port: port, inputStream: &inputStream, outputStream: &outputStream)
-        inputStream.delegate = self
-        outputStream.delegate = self
-        inputStream.schedule(in: .current, forMode: .common)
-        outputStream.schedule(in: .current, forMode: .common)
-        inputStream.open()
-        outputStream.open()
-    }
-
-    func authenticate(username: String, oauthToken: String) {
-        sendMessage("PASS oauth:\(oauthToken)")
-        sendMessage("NICK \(username)")
-    }
-
-    func join(channel: String) {
-        sendMessage("JOIN \(channel)")
-    }
-
-    func sendMessage(_ message: String) {
-        let data = "\(message)\r\n".data(using: .utf8)!
-        _ = data.withUnsafeBytes { outputStream.write($0.bindMemory(to: UInt8.self).baseAddress!, maxLength: data.count) }
-    }
-
-    func listen() {
-        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: 1024)
-        while inputStream.hasBytesAvailable {
-            let bytesRead = inputStream.read(buffer, maxLength: 1024)
-            guard let message = String(bytesNoCopy: buffer, length: bytesRead, encoding: .utf8, freeWhenDone: true) else {
-                continue
-            }
-            handleIncomingMessage(message)
-        }
-    }
-
-    func handleIncomingMessage(_ message: String) {
-        if message.hasPrefix("PING") {
-            respondToPing()
-        } else {
-            // Handle other types of messages
-        }
-    }
-
-    func respondToPing() {
-        sendMessage("PONG :tmi.twitch.tv")
-    }
-}
-
-extension IRCClient: StreamDelegate {
-    func isEqual(_ object: Any?) -> Bool {
-        return false
-    }
-    
-    var hash: Int {
-        return 0
-    }
-    
-    var superclass: AnyClass? {
-        return nil
-    }
-    
-    func `self`() -> Self {
-        return self
-    }
-    
-    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
-        return nil
-    }
-    
-    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
-        return nil
-    }
-    
-    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
-        return nil
-    }
-    
-    func isProxy() -> Bool {
-        return false
-    }
-    
-    func isKind(of aClass: AnyClass) -> Bool {
-        return false
-    }
-    
-    func isMember(of aClass: AnyClass) -> Bool {
-        return false
-    }
-    
-    func conforms(to aProtocol: Protocol) -> Bool {
-        return false
-    }
-    
-    func responds(to aSelector: Selector!) -> Bool {
-        return false
-    }
-    
-    var description: String {
-        return ""
-    }
-    
-    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
-        switch eventCode {
-        case .hasBytesAvailable:
-            if aStream == inputStream {
-                listen()
-            }
-        default:
-            break
-        }
-    }
-}
-    
-func handleTwitch() throws {
-    // Create an instance of TwitchChat
-    let host = "irc.chat.twitch.tv"
-    let port = 6667
-
-    // Twitch username and OAuth token (replace with your own)
-    let username = "SkyEyeBot"
-    let oauthToken = "oauth:0vketpwjlflxtfoiwgs9840yfmqmbg"
-
-    // Twitch channel to join
-    let channel = "#imbestcaptain"
-
-    // Create an instance of IRCClient
-    let ircClient = IRCClient(host: host, port: port)
-
-    // Connect to Twitch IRC server
-    ircClient.connect()
-
-    // Authenticate with Twitch IRC
-    ircClient.authenticate(username: username, oauthToken: oauthToken)
-
-    // Join the Twitch channel
-    ircClient.join(channel: channel)
-    while true {
-        usleep(10000)
-    }
-
-}
-
-
 //Log battle
 func logBattle(eventId: String, captainName: String, scale: String, qttPlayers: String, enmPlayers: String, power: String, enmPower: String, spell: String, enmSpell: String, unit: String, enmUnit: String, outcome: String) -> String {
     
@@ -256,16 +102,25 @@ func updateList(username:String, listId: String) {
 //Play audio cue to start battle
 func playBattle() {
     
-    guard let path = Bundle.main.path(forResource: "whistle.mp3", ofType:"mp3") else {
-        return }
-    let url = URL(fileURLWithPath: path)
-    print(url)
+    guard let url = URL(string: "") else {
+        print("Invalid URL")
+        return
+    }
+
     do {
-        let player = try AVAudioPlayer(contentsOf: url)
+        let soundData = try Data(contentsOf: url)
+        let audioPlayer = try AVAudioPlayer(data: soundData)
+        audioPlayer.volume = 1
         
-        player.play()
-    } catch let error {
-        print(error.localizedDescription)
+        let minuteString = String(format: "%02d", (Int(audioPlayer.duration) / 60))
+        let secondString = String(format: "%02d", (Int(audioPlayer.duration) % 60))
+        print("TOTAL TIMER: \(minuteString):\(secondString)")
+        
+        // Start playback
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    } catch {
+        print(error)
     }
     
     let process = Process()
